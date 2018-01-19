b8446c23528d8d54dd2193dc77584f1f6772ba93
Magnus Danielsen <magnudan@stud.ntnu.no>
2018-01-19 17:50:55 +0100

    Update foo.go

--- Exercise 1/Problem 4/go/foo.go
+++ Exercise 1/Problem 4/go/foo.go
@@ -23,7 +23,7 @@ func main() {
 	                                    // Try doing the exercise both with and without it!
 
     // TODO: Spawn both functions as goroutines
-	
+
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
