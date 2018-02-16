43824a29ed73698eb3318d79ab5cf27dd66e3a50
jennybg <jennybg@stud.ntnu.no>
2018-02-16 16:19:52 +0100

    added driver functions

--- src/IO/hw.go
+++ src/IO/hw.go
@@ -7,65 +7,197 @@ import (
 	"net"
 	"log"
 	"time"
+	"sync"
 )
+
+
+const _pollRate = 20 * time.Millisecond
+
+var _initialized bool = false
+var _numFloors int = 4
+var _mtx sync.Mutex
+var _conn net.Conn
+
+type MotorDirection int
+
+const (
+	MD_Up   MotorDirection = 1
+	MD_Down                = -1
+	MD_Stop                = 0
+)
+
+type ButtonType int
+
+const (
+	BT_HallUp   ButtonType = 0
+	BT_HallDown            = 1
+	BT_Cab                 = 2
+)
+
+type ButtonEvent struct {
+	Floor  int
+	Button ButtonType
+}
+
 
 
 
 
 //init function
-func initHw () {
-
+func initHw (addr strin, numFloors int) {
+	if _initialized {
+		fmt.Println("Driver already initialized!")
+		return
+	}
+	_numFloors = numFloors
+	_mtx = sync.Mutex{}
+	var err error
+	_conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		panic(err.Error())
+	}
+	_initialized = true
 }
 
 
 
 //set functions
 func setElevLights() { //uses setOrderLight() and setFloorIndicatorLight()
-
+	
 }
 
 
-func setDoorLight() {
-
+func setDoorLight(value bool) {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{4, toByte(value), 0, 0})
 }
 
 
-func setFloorIndicatorLight() {
-
+func setFloorIndicatorLight(floor int) {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{3, byte(floor), 0, 0})
 }
 
 
-func setOrderLight() {
-
+func setButtonLight(button ButtonType, floor int, value bool) {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{2, byte(button), byte(floor), toByte(value)})
 }
 
 
-func setMotorDir() {
-
+func setMotorDir(dir MotorDirection) {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{1, byte(dir), 0, 0})
 }
 
 
 
-//read functions
-func readFloor() {
-
+//get functions
+func getFloor() int {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{7, 0, 0, 0})
+	var buf [4]byte
+	_conn.Read(buf[:])
+	if buf[1] != 0 {
+		return int(buf[2])
+	} else {
+		return -1
+	}
 }
 
 
-func readButton() {
-
+func getButton(button ButtonType, floor int) {
+	_mtx.Lock()
+	defer _mtx.Unlock()
+	_conn.Write([]byte{6, byte(button), byte(floor), 0})
+	var buf [4]byte
+	_conn.Read(buf[:])
+	return toBool(buf[1])
 }
 
 
 
 //poll functions
-func pollFloors() {
-
+func pollFloors(receiver chan<- int) {
+	prev := -1
+	for {
+		time.Sleep(_pollRate)
+		v := getFloor()
+		if v != prev && v != -1 {
+			receiver <- v
+		}
+		prev = v
+	}
 }
 
 
-func pollElevButtons() {
-
+func pollElevButtons(receiver chan<- ButtonEvent)  {
+	prev := make([][3]bool, _numFloors)
+	for {
+		time.Sleep(_pollRate)
+		for f := 0; f < _numFloors; f++ {
+			for b := ButtonType(0); b < 3; b++ {
+				v := getButton(b, f)
+				if v != prev[f][b] && v != false {
+					receiver <- ButtonEvent{f, ButtonType(b)}
+				}
+				prev[f][b] = v
+			}
+		}
+	}
 }
+
+
+
+
+//bits, bytes and bools
+func toByte(a bool) byte {
+	var b byte = 0
+	if a {
+		b = 1
+	}
+	return b
+}
+
+
+func toBool(a byte) bool {
+	var b bool = false
+	if a != 0 {
+		b = true
+	}
+	return b
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
